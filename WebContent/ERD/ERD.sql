ff75e0602eac54f4912add7eabc33adc79556755
odh <odh@odh-PC>
2020-01-02 21:32:00 +0900

    8.오다현 erd수정 및 샘플데이터 추가

--- WebContent/ERD/ERD.sql
+++ WebContent/ERD/ERD.sql
@@ -36,6 +36,7 @@ CREATE TABLE COMMENT
 	co_content text,
 	co_name varchar(40) NOT NULL,
 	bo_uid int NOT NULL,
+	co_regdate datetime NOT NULL DEFAULT now(),
 	PRIMARY KEY (co_uid)
 );
 
@@ -48,7 +49,8 @@ CREATE TABLE DESIGNER
 	de_major varchar(40) NOT NULL,
 	de_pictuer varchar(40),
 	sh_uid int NOT NULL,
-	PRIMARY KEY (de_uid)
+	PRIMARY KEY (de_uid),
+	UNIQUE (sh_uid)
 );
 
 
@@ -57,6 +59,7 @@ CREATE TABLE REPLY
 	re_uid int NOT NULL AUTO_INCREMENT,
 	re_content text,
 	co_uid int NOT NULL,
+	re_regdate datetime NOT NULL DEFAULT now(),
 	PRIMARY KEY (re_uid)
 );
 
@@ -75,18 +78,26 @@ CREATE TABLE SERVICE
 CREATE TABLE SHOP
 (
 	sh_uid int NOT NULL AUTO_INCREMENT,
-	sh_id int NOT NULL,
-	sh_location varchar(80) NOT NULL,
-	sh_location_lat varchar(40) NOT NULL,
-	sh_location_lng varchar(40) NOT NULL,
+	sh_id varchar(40) NOT NULL,
+	sh_pw varchar(15) NOT NULL,
+	sh_no_id double NOT NULL,
+	sh_name varchar(20) NOT NULL,
+	sh_location varchar(80),
+	sh_location_lat varchar(40),
+	sh_location_lng varchar(40),
 	sh_picture1 varchar(40),
 	sh_picture2 varchar(40),
 	sh_picture3 varchar(40),
 	sh_picture4 varchar(40),
 	sh_picture5 varchar(40),
-	sh_identify int DEFAULT 2,
+	sh_dayoff1 int CHECK (sh_dayoff1 >=1 and  sh_dayoff1 <= 7),
+	sh_dayoff2 int CHECK(sh_dayoff2>= 1 AND sh_dayoff2<=7),
+	sh_starttime int DEFAULT 9,
+	sh_endtime int DEFAULT 21,
+	num_identify int DEFAULT 2,
 	PRIMARY KEY (sh_uid),
-	UNIQUE (sh_id)
+	UNIQUE (sh_id),
+	UNIQUE (sh_no_id)
 );
 
 
@@ -94,10 +105,10 @@ CREATE TABLE USER
 (
 	use_uid int NOT NULL AUTO_INCREMENT,
 	use_id varchar(40) NOT NULL,
+	use_pw varchar(15) NOT NULL,
 	use_name varchar(40) NOT NULL,
-	use_pw varchar(40) NOT NULL,
-	use_phoneNum int NOT NULL,
-	use_identify int NOT NULL DEFAULT 1,
+	use_phoneNum varchar(11) NOT NULL,
+	num_identify int DEFAULT 1,
 	PRIMARY KEY (use_uid),
 	UNIQUE (use_id),
 	UNIQUE (use_phoneNum)
@@ -162,20 +173,41 @@ ALTER TABLE BOOK
 	ON DELETE RESTRICT
 ;
 
-/* Sample Data */
+
+
+/** Sample Data **/
 INSERT INTO USER(
-use_uid,
-use_id,
-use_pw,
-use_name,
-use_phoneNum
+	use_id,
+	use_pw,
+	use_name,
+	use_phoneNum
 )
-VALUES(0, 'test01', '1234', '손님용', '01000000000');
+VALUES('test01', '1234', '손님용', '01000000000');
 
-SELECT * FROM USER;
+INSERT INTO SHOP(
+	sh_id,
+	sh_pw,
+	sh_no_id,
+	sh_name
+)
+VALUES('store01', '1234', 1111111111, '매장용');
 
+INSERT INTO DESIGNER
+(
+	de_name,
+	de_career,
+	de_major,
+	sh_uid
+)
+VALUES('디자이너1', 5, '염색', 1);
 
-
-
+INSERT INTO SERVICE
+(
+	ser_name,
+	ser_price,
+	ser_time,
+	sh_uid
+)
+VALUES('염색', 100000, 30000, 1);
 
 
